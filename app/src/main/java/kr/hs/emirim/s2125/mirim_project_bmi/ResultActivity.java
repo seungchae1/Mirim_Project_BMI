fb4ef7f5c8d03a0a0533a207fd4671eb4b574ef4
seungchae1 <s2125@e-mirim.hs.kr>
2022-07-28 10:53:05 +0900

    HOME버튼 추가

--- app/src/main/java/kr/hs/emirim/s2125/mirim_project_bmi/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/s2125/mirim_project_bmi/ResultActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -52,5 +54,13 @@ public class ResultActivity extends AppCompatActivity {
         textName.setText(name+"님의 BMI지수는 ");
         textBMi.setText(String.format("%.2f",Bmi)+"입니다");
         textWeight.setText(wResult);
+
+        Button btnHome = findViewById(R.id.btn_home);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
