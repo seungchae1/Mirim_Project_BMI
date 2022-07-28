1a183e2a703c3457bcc6ff88108f50bbc54b4689
seungchae1 <s2125@e-mirim.hs.kr>
2022-07-28 10:26:54 +0900

    BMI지수 계산, ResultActivity에 출력

--- app/src/main/java/kr/hs/emirim/s2125/mirim_project_bmi/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2125/mirim_project_bmi/MainActivity.java
@@ -2,13 +2,33 @@ package kr.hs.emirim.s2125.mirim_project_bmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText Editname, Editweight, Edittall;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnResult= findViewById(R.id.btn_result);
+        Editname= findViewById(R.id.edit_name);
+        Editweight= findViewById(R.id.edit_weight);
+        Edittall= findViewById(R.id.edit_tall);
+        btnResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ResultActivity.class);
+                intent.putExtra("name",Editname.getText().toString());
+                intent.putExtra("weight",Editweight.getText().toString());
+                intent.putExtra("tall",Edittall.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/kr/hs/emirim/s2125/mirim_project_bmi/ResultActivity.java
@@ -0,0 +1,56 @@
+package kr.hs.emirim.s2125.mirim_project_bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        TextView textName = findViewById(R.id.text_name);
+        TextView textBMi = findViewById(R.id.text_bmi);
+        TextView textWeight = findViewById(R.id.text_weight);
+        ImageView imgV = findViewById(R.id.bmi_img);
+
+        Intent  intent= getIntent();
+        String name =intent.getStringExtra("name");
+        double weight = Integer.parseInt(intent.getStringExtra("weight"));
+        double tall = Integer.parseInt(intent.getStringExtra("tall"));
+
+        double Bmi= weight / Math.pow(tall/100, 2);
+        String wResult="";
+
+        if(Bmi>30){
+            wResult="심한 과체중";
+            imgV.setImageResource(R.drawable.bmi5);
+        }
+        else if(Bmi>25) {
+            wResult="과체중";
+            imgV.setImageResource(R.drawable.bmi4);
+        }
+        else if(Bmi>18.5) {
+            wResult="표준체중";
+            imgV.setImageResource(R.drawable.bmi3);
+        }
+        else if(Bmi>15){
+            wResult="저체중";
+            imgV.setImageResource(R.drawable.bmi2);
+        }
+        else {
+            wResult="심한 저체중";
+            imgV.setImageResource(R.drawable.bmi1);
+        }
+
+        textName.setText(name+"님의 BMI지수는 ");
+        textBMi.setText(String.format("%.2f",Bmi)+"입니다");
+        textWeight.setText(wResult);
+    }
+}
